f9c6dc33540c4dfbc646cf177c4a75015bd2e86d
rawindigangoda-gwd <rawindigangoda@gmail.com>
2026-03-31 23:03:02 +0530

    Add files via upload

---
+++ InputDemo.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+class InputDemo{
+	public static void main(String arg[]){
+		Scanner sc = new Scanner(System.in);
+		
+		int a=0;
+		System.out.println("The value of a is: "+a);
+		
+		System.out.println("Enter the value for a: ");
+		a=sc.nextInt();
+		
+		System.out.println("The value of a is: "+a);
+	}
+}
